39d965680c86286ba3080e0a6c23e019bd6d339f
NikosiTech <nicolas.garnier@epita.fr>
2015-02-08 16:08:07 +0100

    Site

---
+++ IA.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IA : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		agent.SetDestination(target.position);
+	}
+}
---
+++ objcollision.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+public class objcollision : MonoBehaviour {
+
+
+	[SerializeField]
+	private Rigidbody targetRigidbody;
+	public string obj_oncollide;
+	public string obj_destroy;
+
+	
+
+	void OnCollisionEnter(Collision objet)
+	{
+		if (objet.gameObject.name == obj_destroy) 
+		{
+			Destroy(targetRigidbody.gameObject);
+		}
+		/*else
+		{
+			action();
+		}
+
+	}
+
+	void action()
+	{
+		float moveSpeed = 10f;
+		transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);*/
+
+	}
+}
+
